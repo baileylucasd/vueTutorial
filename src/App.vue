e668fe8305785a8657b667e17a7a47c636a3fdef
Luke Bailey <baileylucasd@gmail.com>
2021-03-16 21:12:01 -0400

    Vue Tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
+    <h1>"VueJS Experiment by {{ me }}"</h1>
+    <h2>Personal Info
+      Name: {{myInfo.name}}<br/>
+      Semester: {{myInfo.semester}} {{myInfo.year}}<br/>
+      Courses I am taking
+      <ol>
+        <li v-for="(c,pos) in myInfo.courses" :key="pos">{{c.name}}
+          ({{c.credits}} credits)</li>
+      </ol>
+    </h2>
+    <table>
+      <tr><td>Course</td><td>Credits</td></tr>
+      <tr v-for="(c,pos) in myInfo.courses" :key="pos">
+        <td>{{c.name}}</td>
+        <td>{{c.credits}}</td>
+      </tr>
+    </table>
+    <CourseCard v-for="(c,pos) in myInfo.courses" :key="pos"
+  v-bind:name="c.name" v-bind:num-credits="c.credits"></CourseCard>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!--HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/-->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import CourseCard from "./components/CourseCard.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    HelloWorld, CourseCard
   },
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+  readonly me = "Lucas Bailey";
+  readonly myInfo = {
+    name: "Lucas Bailey",
+    semester: "Winter",
+    year: 2021,
+    courses: [
+      { name: "CIS380 Game Design", credits: 3},
+      { name: "CIS371 Web Application Development", credits: 3},
+    ],
+  };
+}
 </script>
 
 <style>
@@ -22,7 +54,6 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/CourseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="crs">
+    <h3>Course: {{name}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class CourseCard extends Vue {
+  @Prop() readonly numCredits!: number;
+  @Prop() readonly name!: string;
+}
+</script>
+
+<style>
+#crs {
+  display: inline-block;
+  border: 2px solid hsl(30, 90%, 50%);
+  border-radius: 0.5em;
+  padding: 1em;
+  margin: 0.6em;
+  box-shadow: 3px 5px 6px 7px hsla(30, 55%, 50%, 0.4);
+}
+</style>
